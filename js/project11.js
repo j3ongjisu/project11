41ee384104e2f876b3320e0db753d6b127621798
j3ongjisu <wjdwltn911@naver.com>
2023-07-17 18:13:40 +0900

    230717

---
+++ js/project11.js
@@ -0,0 +1,88 @@
+$(function () {
+    $('#fl').on('change', function () {
+        const lnk = $(this).val();
+        console.log(lnk, '바뀜');
+        lnk && window.open(lnk);
+    });
+
+
+    $(window).on('scroll', function () {
+        // 변수에 스크롤한 양을 담는다.
+        let sct = $(window).scrollTop();
+        console.log(sct)
+        if (sct > 0) {
+            $('.header').addClass('on')
+        } else {
+            $('.header').removeClass('on')
+        }
+
+    })
+
+
+
+    $('.main_slide').on('init afterChange', function (e, s, c) {
+        const current = $('.main_slide .slick-current');
+        current.addClass('on').siblings().removeClass('on');
+
+        console.log(s.slideCount);
+
+        $('.main_visual .menu li').eq(0).addClass('on');
+        $('.main_visual .menu li').eq(c).addClass('on')
+            .siblings().removeClass('on');
+    });
+
+    $('.main_slide').slick({
+        //autoplay: true,
+        autoplaySpeed: 5000,
+        speed: 1000,
+        pauseOnHover: false,
+        fade: true,
+        arrows: false,
+    });
+
+
+    $('.main_visual .arrows .left').on('click', function () {
+        $('.main_slide').slick('slickPrev')
+    });
+    $('.main_visual .arrows .right').on('click', function () {
+        $('.main_slide').slick('slickNext')
+    });
+
+    $('.main_visual .menu li').on('click', function (e) {
+        e.preventDefault();
+        const idx = $(this).index(); // 0,1,2
+        $('.main_slide').slick('slickGoTo', idx);
+    })
+
+
+    //메인 슬라이드 스크롤 아래 버튼
+    $('.scr').on('click', function (e) {
+        e.preventDefault();
+        const st = $(this.hash).offset().top;
+        console.log(st);
+        $('html, body').animate({ scrollTop: st - 100 }, 600)
+    });
+
+    $('.product_slide').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 3,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 3,
+                }
+            }
+        ]
+    });
+
+    $('.main_special .arrows .left').on('click', function () {
+        $('.product_slide').slick('slickPrev');
+    });
+    $('.main_special .arrows .right').on('click', function () {
+        $('.product_slide').slick('slickNext');
+    });
+
+
+})
